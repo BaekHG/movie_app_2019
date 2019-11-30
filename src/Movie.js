1bd4c3a13c4caf8f7b12abfa2de25378eb528b95
brobaik <hhyungi94@gmail.com>
2019-12-01 02:28:50 +0900

    show_movies_data

--- src/Movie.js
+++ src/Movie.js
@@ -1,11 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({id,year,title,summary,poster}){
-    return <h1>{title}</h1>
+function Movie({ id, year, title, summary, poster }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title}></img>
+            <div className="movie__data">
+                <h3 className="movie__title" 
+                style={{
+                    backgroundColor: "blue"
+                    
+                    }}>{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie__summary">{summary}</p>
+            </div>
+        </div>
+    )
 }
 
-Movie.propTypes ={
+Movie.propTypes = {
     id: PropTypes.number.isRequired,
     year: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
